cd9f1c3f6e84a778050fb0c263c9efe635b5caaa
Tran Nguyen Khai Luan <tnkluan23@clc.fitus.edu.vn>
2025-12-04 23:50:03 +0700

    fix header

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -62,7 +62,7 @@ const Header: React.FC = () => {
           </Link>
 
           {/* Global Search Bar */}
-          <form onSubmit={handleSearch} className="flex-1 max-w-2xl">
+          <form onSubmit={handleSearch} className="flex-1">
             <div className="relative">
               <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
               <Input
